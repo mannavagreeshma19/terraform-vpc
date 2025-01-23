db0158c7963ba20f98235815db169bbecf00e0d2
Greeshma Mannava <mannavag8@gmail.com>
2025-01-23 18:21:10 -0500

    adding ec2 instance

--- ec2.tf
+++ ec2.tf
@@ -6,4 +6,14 @@ resource "aws_instance" "main_instance"{
     tags={
         Name="main_instance"
     }
+
+    user_data = <<-EOF
+    #!/bin/bash
+    
+    sudo apt update -y
+    sudo apt install -y unzip curl
+    curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip"
+    unzip awscliv2.zip
+    sudo ./aws/install
+  EOF
 }
